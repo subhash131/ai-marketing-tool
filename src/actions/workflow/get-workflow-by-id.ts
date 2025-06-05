955d8f5e035714c63fca75da69d90bda51584783
subhash131 <subhashnayak131@gmail.com>
2025-06-05 14:49:55 +0530

    fix(getWorkflowById): add return type

--- src/actions/workflow/get-workflow-by-id.ts
+++ src/actions/workflow/get-workflow-by-id.ts
@@ -1,16 +1,18 @@
 "use server";
 
+import { Workflow } from "@/types/workflow";
+
 export const getWorkflowById = async ({
   workflowId,
 }: {
   workflowId: string;
-}) => {
+}): Promise<Workflow> => {
   try {
     const BACKEND_URL = process.env.BACKEND_URL;
     const res = await fetch(`${BACKEND_URL}/workflow/${workflowId}`);
     if (res.ok) {
       const data = await res.json();
-      return { data, status: 200 };
+      return data;
     } else {
       throw new Error("Failed to Fetch Workflow");
     }
